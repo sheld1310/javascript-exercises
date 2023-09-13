6fb638f25d89f5b8accb55297a712a95f8090c77
sheld1310 <140619430+sheld131@users.noreply.github.com>
2023-09-13 23:06:07 +0100

    Assign variable to empty string and loop 3 times
    Make a new variable called outcome, and assign it an empty string
    Then create for loop to increment num by 1 whilst it's less than three
    Finally add outcome variable to string argument on each iteration of
    the loop. Then outside the loop return the output. What this will do
    is return String 3 times 'hey' resulting in the outcome of 'heyheyhey'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+let output = '';
+for (let num = 0; num < 3; num++){  
+    output += string;
+}
+return output;
 };
 
 // Do not edit below this line
