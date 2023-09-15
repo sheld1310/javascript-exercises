dfd7c95efde589cf8bde5115eecef4098078b9c5
sheld1310 <140619430+sheld131@users.noreply.github.com>
2023-09-16 00:23:38 +0100

    Shorten code by reducing two else if into a single else if
    Combine the else if condition to check if leapYear1 is true, and
    for notLeapYear to be false. Return true if both these conditions
    match. We must make sure the condition for leapYear2 is checked first
    because this is the definite rule, if the year is divisible by 400,
    it is true every time, which is why this condition must be checked
    first.
    Finally add a else statement to return false if neither of
    the if or else if conditions are true. Doing this will satisfy
    all the test conditions.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,21 +1,21 @@
 const leapYears = function(year) {
-
+    // set variables to store the modula of year
     let leapYear1 = year % 4 == 0
     let leapYear2 = year % 400 == 0;
     let notLeapYear = year % 100 == 0;
+
+    //add conditions that returns true or false for each
+    //variable
     if (leapYear2){
         return true;
     }
-    else if (notLeapYear){
-        return false;
-    }
-    else if (leapYear1){
+    else if (leapYear1 && !(notLeapYear)){
         return true;
     }
     else {
         return false;
     }
-};
 
+    }
 // Do not edit below this line
 module.exports = leapYears;
