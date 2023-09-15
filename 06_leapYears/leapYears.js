6435af51be0ae278dcfe471dff6e5e729e41c762
sheld1310 <140619430+sheld131@users.noreply.github.com>
2023-09-16 00:15:20 +0100

    Add variables to store the modula for each year outcome
    Set 3 variables that store the outcome for year. Use modula to
    do store this for each. We are checking for each if year is
    divisible by 4, 400, and 100.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let leapYear1 = year % 4 == 0
+    let leapYear2 = year % 400 == 0;
+    let notLeapYear = year % 100 == 0;
+    if (leapYear2){
+        return true;
+    }
+    else if (notLeapYear){
+        return false;
+    }
+    else if (leapYear1){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
