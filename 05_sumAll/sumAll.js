c737364dd5e7d3db15e6b032c96546d8f323f7bc
sheld1310 <140619430+sheld131@users.noreply.github.com>
2023-09-15 22:22:34 +0100

    Satisfy the final 3 test conditions
    Improve the logic for checking whether num1, num2 is greater than
    or less to by adding the Math.max and Math.min object. In order to
    make this work we will need to store num1 under second variable "temp"
    this will prevent the original value of num1 being
    changed during the min.max section. Finally add a condition that checks
    if the number entered is positive and to only accept an integer.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,20 @@
 const sumAll = function(num1, num2) {
-    totalSum = 0;
+
+    if (num1 < 0 || typeof num1 !== 'number'|| num2 < 0 || typeof num2 !== 'number'){
+        return 'ERROR';
+    }
+
+    let totalSum = 0;
+    let temp = num1;
+    num1 = Math.min(temp, num2);
+    num2 = Math.max(temp, num2);
     
     if (num1 < num2){
         for (let i = num1; i <= num2; i++){
             totalSum += i;
         }
     }
-        else if (num1 > num2){
+        else{
         for (let i = num2; i <= num1; i++){
             totalSum += i;
         }
@@ -16,4 +24,4 @@ const sumAll = function(num1, num2) {
 
 // Do not edit below this line
 module.exports = sumAll;
-//npm test sumAll.spec.js
+
