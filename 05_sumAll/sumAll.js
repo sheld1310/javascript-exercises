0ec6be5c3005936a36422ce6a7de3c2aee7293db
sheld1310 <140619430+sheld131@users.noreply.github.com>
2023-09-15 20:15:01 +0100

    Satisfy first 3 tests, by adding two conditions
    Add two conditions to look for if num1 is less than num2,
    and second condition for if num1 is greater than num2. Then in
    both conditions, add a loop to increment i by 1 on each loop. Make
    total sum equal to, total sum + i; to satisfy the first 3 tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    totalSum = 0;
+    
+    if (num1 < num2){
+        for (let i = num1; i <= num2; i++){
+            totalSum += i;
+        }
+    }
+        else if (num1 > num2){
+        for (let i = num2; i <= num1; i++){
+            totalSum += i;
+        }
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+//npm test sumAll.spec.js
